272fda00cccc9c650250b60fc3f8944b9ee60b50
Will Gibson <will.gibson@walterego.co.uk>
2020-08-25 14:39:58 +0100

    Sort out the docker helper alias command paths

--- src/components/zshrc/aliases/docker.sh
+++ src/components/zshrc/aliases/docker.sh
@@ -1,11 +1,10 @@
 #!/bin/bash
 
-alias dss="docker-sync start"
-alias dsc="docker-sync clean"
-alias dv="docker volume"
-alias docker-cleanup="~/linux-setup-will-01/DockerScripts/docker-cleanup"
-alias docker-brutal-cleanup="~/linux-setup-will-01/DockerScripts/docker-brutal-cleanup"
+# shellcheck disable=SC2164
+basePath="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/../../"
+
+alias docker-cleanup="${basePath}scripts/docker/docker-cleanup"
+alias docker-brutal-cleanup="${basePath}scripts/docker/docker-brutal-cleanup"
 alias docker-pull-all="docker images |grep -v REPOSITORY|awk '{print $1}'|xargs -L1 docker pull"
-alias docker-really-brutal-cleanup="~/linux-setup-will-01/DockerScripts/docker-really-brutal-cleanup"
-alias docker-disk-usage="sudo ~/Sites/BiffBangPow/docker-disk-usage/bin/docker-disk-usage --detailed --include-mounts"
-alias sda="source docker/aliases"
+alias docker-really-brutal-cleanup="${basePath}scripts/docker/docker-really-brutal-cleanup"
+
