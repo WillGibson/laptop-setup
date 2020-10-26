9e933853b85b34d4c5f87d3e05408078fe1dd021
Will Gibson <will.gibson@walterego.co.uk>
2020-10-26 17:33:46 +0000

    Whatever Mon Oct 26 17:33:46 GMT 2020

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,9 +5,13 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](http://keepachangelog.com/en/1.0.0/)
 and this project adheres to [Semantic Versioning](http://semver.org/spec/v2.0.0.html).
 
-## [unreleased]
+## [0.1.5] - 2020-10-26
 ### Added
 - GitHub Actions pipeline
+- Install VS Code
+- Install Slack
+- Install Microsoft Teams
+- Install Google Chrome
 ### Changed
 - Split up the commands and extract more things to one liners
 
--- src/setup.sh
+++ src/setup.sh
@@ -21,65 +21,65 @@ source "${basePath}/components/commands/zshrc.sh"
 ensure_docker_not_running
 ensure_git_name_and_email_are_set
 
-#append_to_zshrc "# The rest should have been added by laptop-setup..."
-#
-#pull_latest_laptop_setup_code
-#
-#set +e && ensure_homebrew_is_installed_and_up_to_date && set -e
-#
-## Terminal
-#brew reinstall iterm2
-#ensure_zsh_is_installed
-#brew reinstall zsh zsh-completions
-#ensure_ohmyzsh_is_installed
-#ensure_zsh_autosuggestions_are_installed
-#ensure_zsh_correction_is_used
-#ensure_zsh_completion_waiting_dots_are_used
-#ensure_correct_ohmyzsh_theme_is_used "${basePath}/components/ohmyzsh/willgibson.zsh-theme" "willgibson"
-#update_file_line_in_situ ~/.zshrc 'plugins=(git)' 'plugins=(docker git zsh-autosuggestions)'
-#
-## Git
-#ensure_git_name_and_email_env_vars_are_exported_in_zshrc
-#brew reinstall git
-#git config --global pull.ff only
-#
-## For signing Git commits
-#brew reinstall gpg2
-#brew reinstall pinentry-mac
-#
-## PHP
-#ensure_php_is_installed
-#brew reinstall composer
-#
-## NVM & Node.js
-#ensure_nvm_is_installed
-#nvm install --lts
-#
-## Java etc.
-#brew reinstall java11
-#brew reinstall maven
-#brew reinstall gradle
-#brew tap pivotal/tap
-#brew reinstall springboot
-#
-## Docker etc.
-#rm -f /usr/local/bin/docker
-#brew reinstall docker
-#rm -f /usr/local/bin/kubectl
-#brew reinstall kubectl
-#brew reinstall minikube
-#
-#brew reinstall awscli
-#
-#rm -rf /Applications/IntelliJ\ IDEA.app
-#brew reinstall intellij-idea
+append_to_zshrc "# The rest should have been added by laptop-setup..."
+
+pull_latest_laptop_setup_code
+
+set +e && ensure_homebrew_is_installed_and_up_to_date && set -e
+
+# Terminal
+brew reinstall iterm2
+ensure_zsh_is_installed
+brew reinstall zsh zsh-completions
+ensure_ohmyzsh_is_installed
+ensure_zsh_autosuggestions_are_installed
+ensure_zsh_correction_is_used
+ensure_zsh_completion_waiting_dots_are_used
+ensure_correct_ohmyzsh_theme_is_used "${basePath}/components/ohmyzsh/willgibson.zsh-theme" "willgibson"
+update_file_line_in_situ ~/.zshrc 'plugins=(git)' 'plugins=(docker git zsh-autosuggestions)'
+
+# Git
+ensure_git_name_and_email_env_vars_are_exported_in_zshrc
+brew reinstall git
+git config --global pull.ff only
+
+# For signing Git commits
+brew reinstall gpg2
+brew reinstall pinentry-mac
+
+# PHP
+ensure_php_is_installed
+brew reinstall composer
+
+# NVM & Node.js
+ensure_nvm_is_installed
+nvm install --lts
+
+# Java etc.
+brew reinstall java11
+brew reinstall maven
+brew reinstall gradle
+brew tap pivotal/tap
+brew reinstall springboot
+
+# Docker etc.
+rm -f /usr/local/bin/docker
+brew reinstall docker
+rm -f /usr/local/bin/kubectl
+brew reinstall kubectl
+brew reinstall minikube
+
+brew reinstall awscli
+
+rm -rf /Applications/IntelliJ\ IDEA.app
+brew reinstall intellij-idea
 
 brew reinstall visual-studio-code
 
-#rm -rf /Applications/Postman.app
-#brew reinstall postman
-#
-#brew reinstall tree
+rm -rf /Applications/Postman.app
+brew reinstall postman
+
+brew reinstall tree
 
 # Likely to bork if checksum does not match after a Chrome release
 # not reflected in the cask, so we're letting it fail gracefully
@@ -89,16 +89,16 @@ brew reinstall slack
 
 brew reinstall microsoft-teams
 
-## Because life without music is not living
-#rm -rf /Applications/Spotify.app
-#brew reinstall spotify
-#
-#append_to_zshrc "source ${basePath}/components/zshrc/aliases/miscellaneous.sh"
-#append_to_zshrc "source ${basePath}/components/zshrc/aliases/git.sh" 1
-#append_to_zshrc "source ${basePath}/components/zshrc/aliases/docker.sh" 1
-#
-#append_to_zshrc "export GPG_TTY=$\(tty\)"
-#
-#fancy_echo "To reload profile now please run...\nsource ~/.zshrc"
-#
-#fancy_echo "You may still need to carry out some manual steps, these are documented at...\nhttps://github.com/WillGibson/laptop-setup#what-it-wont-do-for-you-yet\n"
+# Because life without music is not living
+rm -rf /Applications/Spotify.app
+brew reinstall spotify
+
+append_to_zshrc "source ${basePath}/components/zshrc/aliases/miscellaneous.sh"
+append_to_zshrc "source ${basePath}/components/zshrc/aliases/git.sh" 1
+append_to_zshrc "source ${basePath}/components/zshrc/aliases/docker.sh" 1
+
+append_to_zshrc "export GPG_TTY=$\(tty\)"
+
+fancy_echo "To reload profile now please run...\nsource ~/.zshrc"
+
+fancy_echo "You may still need to carry out some manual steps, these are documented at...\nhttps://github.com/WillGibson/laptop-setup#what-it-wont-do-for-you-yet\n"
