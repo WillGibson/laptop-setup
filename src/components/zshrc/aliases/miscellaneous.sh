516ff0e02632836a8e2b6c6290cc2f7b8b98923d
Will Gibson <will.gibson@walterego.co.uk>
2020-10-28 10:16:29 +0000

    Tidy up some zshrc aliases

--- src/components/zshrc/aliases/miscellaneous.sh
+++ src/components/zshrc/aliases/miscellaneous.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
 
-alias dep="./vendor/bin/dep -vvv"
 alias grep="grep --color=auto"
-alias devip="sudo ip addr add 10.254.254.254/24 brd + dev docker0 label docker0:1"
 alias mine="sudo chown -R will:will ./"
-alias saydone="spd-say --volume=100 --rate=-50 \"Your long time consuming task has completed Mr Gibson\""
+alias saydone="say \"Your long time consuming task has completed Mr Gibson\""
 alias vihosts="sudo vi /etc/hosts"
