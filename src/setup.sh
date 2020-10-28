9a897a6479e312f143a1c60f4a8dd351384fff50
Will Gibson <will.gibson@madetech.com>
2020-10-28 16:59:47 +0000

    Whatever Wed Oct 28 16:59:47 GMT 2020

--- src/setup.sh
+++ src/setup.sh
@@ -27,7 +27,7 @@ echo_heading "Preflight checks"
 ensure_docker_not_running
 ensure_git_name_and_email_are_set
 
-#pull_latest_laptop_setup_code
+pull_latest_laptop_setup_code
 
 set +e && ensure_homebrew_is_installed_and_up_to_date && set -e
 
@@ -46,64 +46,64 @@ update_file_line_in_situ ~/.zshrc 'plugins=(git)' 'plugins=(docker git zsh-autos
 # Git
 echo_heading "Install Git"
 ensure_git_name_and_email_env_vars_are_exported_in_zshrc
-#installApplicationHomebrewStyle "git" 1
-#git config --global pull.ff only
-#ensure_symlink_exists "${basePath}/components/static_files/.gitignore_global" ~/.gitignore_global
-#git config --global core.excludesfile ~/.gitignore_global
-#
-## GPG for signing Git commits
-#installApplicationHomebrewStyle "gpg2"
-#installApplicationHomebrewStyle p"inentry-mac"
-#
-## Python3
-#brew unlink python@3.8
-#installApplicationHomebrewStyle "python"
-#
-## PHP
-#ensure_php_is_installed
-#installApplicationHomebrewStyle "composer"
+installApplicationHomebrewStyle "git" 1
+git config --global pull.ff only
+ensure_symlink_exists "${basePath}/components/static_files/.gitignore_global" ~/.gitignore_global
+git config --global core.excludesfile ~/.gitignore_global
+
+# GPG for signing Git commits
+installApplicationHomebrewStyle "gpg2"
+installApplicationHomebrewStyle p"inentry-mac"
+
+# Python3
+brew unlink python@3.8
+installApplicationHomebrewStyle "python"
+
+# PHP
+ensure_php_is_installed
+installApplicationHomebrewStyle "composer"
 
 # NVM & Node.js
 ensure_nvm_is_installed
-#echo_heading "Install current long term support version of Node.js"
-#echo_empty_line
-#nvm install --lts
-#
-## Java etc.
-#installApplicationHomebrewStyle "java11"
-#installApplicationHomebrewStyle "maven"
-#installApplicationHomebrewStyle "gradle"
-#brew tap pivotal/tap
-#installApplicationHomebrewStyle "springboot"
-#
-## Docker etc.
-#rm -f /usr/local/bin/docker
-#rm -rf /usr/local/Cellar/docker/
-#installApplicationHomebrewStyle "docker"
-#rm -f /usr/local/bin/kubectl
-#installApplicationHomebrewStyle "kubectl"
-#installApplicationHomebrewStyle "minikube"
-#
-## AWS
-#rm -f /usr/local/bin/aws
-#rm -f /usr/local/bin/aws_completer
-#installApplicationHomebrewStyle "awscli"
-#
-#installApplicationMacStyle "intellij-idea" "IntelliJ IDEA"
-#
-#installApplicationMacStyle "visual-studio-code" "Visual Studio Code"
-#
-#installApplicationMacStyle "postman" "Postman"
-#
-#installApplicationHomebrewStyle "tree"
-#
-#installApplicationMacStyle "google-chrome" "Google Chrome"
-#
-#installApplicationMacStyle "slack" "Slack"
-#
-#installApplicationMacStyle "microsoft-teams" "Microsoft Teams" "sudo"
-#
-#installApplicationMacStyle "spotify" "Spotify"
+echo_heading "Install current long term support version of Node.js"
+echo_empty_line
+nvm install --lts
+
+# Java etc.
+installApplicationHomebrewStyle "java11"
+installApplicationHomebrewStyle "maven"
+installApplicationHomebrewStyle "gradle"
+brew tap pivotal/tap
+installApplicationHomebrewStyle "springboot"
+
+# Docker etc.
+rm -f /usr/local/bin/docker
+rm -rf /usr/local/Cellar/docker/
+installApplicationHomebrewStyle "docker"
+rm -f /usr/local/bin/kubectl
+installApplicationHomebrewStyle "kubectl"
+installApplicationHomebrewStyle "minikube"
+
+# AWS
+rm -f /usr/local/bin/aws
+rm -f /usr/local/bin/aws_completer
+installApplicationHomebrewStyle "awscli"
+
+installApplicationMacStyle "intellij-idea" "IntelliJ IDEA"
+
+installApplicationMacStyle "visual-studio-code" "Visual Studio Code"
+
+installApplicationMacStyle "postman" "Postman"
+
+installApplicationHomebrewStyle "tree"
+
+installApplicationMacStyle "google-chrome" "Google Chrome"
+
+installApplicationMacStyle "slack" "Slack"
+
+installApplicationMacStyle "microsoft-teams" "Microsoft Teams" "sudo"
+
+installApplicationMacStyle "spotify" "Spotify"
 
 echo_heading "Include aliases in .zshrc"
 append_to_zshrc_parts "source ${basePath}/components/zshrc/aliases/miscellaneous.sh"
@@ -116,13 +116,13 @@ append_to_zshrc_parts "export GPG_TTY=$\(tty\)"
 append_to_zshrc "# Added by laptop-setup..."
 append_to_zshrc "source ~/.zshrc_parts_from_laptop_setup.sh" 1
 
-#echo_heading "Run brew cleanup"
-#echo_empty_line
-#set +e && brew cleanup && set -e
-#
-#echo_heading "Run brew doctor"
-#echo_empty_line
-#set +e && brew doctor && set -e
+echo_heading "Run brew cleanup"
+echo_empty_line
+set +e && brew cleanup && set -e
+
+echo_heading "Run brew doctor"
+echo_empty_line
+set +e && brew doctor && set -e
 
 echo_heading "A couple of other things to note"
 echo_line "\n1) To reload profile now please run...\n\nsource ~/.zshrc"
