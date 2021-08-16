d8426393b68c5719572d49a173076af48ff81b2b
Will Gibson <will.gibson.contractor@dvla.gov.uk>
2021-08-16 17:20:06 +0100

    ZSH_AUTOSUGGEST_MANUAL_REBIND=false

--- src/setup.sh
+++ src/setup.sh
@@ -53,6 +53,7 @@ if include "terminal"; then
     ensure_zsh_completion_waiting_dots_are_used
     ensure_correct_ohmyzsh_theme_is_used "${basePath}/components/ohmyzsh/willgibson.zsh-theme" "willgibson"
     update_file_line_in_situ ~/.zshrc 'plugins=(git)' 'plugins=(docker git kubectl zsh-autosuggestions)'
+    append_to_zshrc_parts "ZSH_AUTOSUGGEST_MANUAL_REBIND=false" # This might need to go before it loads ohmyzsh
 fi
 
 if include "git"; then
