3a243695adef2db44c0fec5b13ffe9743b0bb416
Will Gibson <will.gibson.contractor@dvla.gov.uk>
2022-09-08 11:54:47 +0100

    Correct the install check string

--- src/components/commands/additionalCommands.sh
+++ src/components/commands/additionalCommands.sh
@@ -9,7 +9,10 @@ additionalCommands() {
     fi
 
     commands=$(jq ".additionalCommands.$stage" "$configFile")
-    if [ "$commands" != null ]; then
+    echo "${commands}"
+    if [ "$commands" == null ]; then
+        echo_heading "There are no additional commands for stage $stage"
+    else
         echo_heading "Additional commands for stage $stage"
         for base64Command in $(jq -r '.[] | @base64' <<< "$commands"); do
             command=$(echo "$base64Command"  | base64 --decode)
