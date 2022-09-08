daeb3a4514c95ece8da54d6c766c0c882ac8dac9
Will Gibson <will.gibson.contractor@dvla.gov.uk>
2022-09-08 16:53:38 +0100

    ensure_jenv_is_installed

--- src/setup.sh
+++ src/setup.sh
@@ -91,9 +91,7 @@ if include "node"; then
 fi
 
 if include "java"; then
-    installApplicationHomebrewStyle "jenv"
-    append_to_zshrc_parts 'export PATH="$HOME/.jenv/bin:$PATH"'
-    append_to_zshrc_parts 'eval "$(jenv init -)"'
+    ensure_jenv_is_installed
     installApplicationHomebrewStyle "java11"
     installApplicationHomebrewStyle "openjdk@17"
     installApplicationHomebrewStyle "maven"
