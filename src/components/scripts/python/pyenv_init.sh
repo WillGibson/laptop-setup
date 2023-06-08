704916ef76721ab6ccdb4c18d45cdf45fbc0b55e
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2023-06-08 11:32:40 +0100

    Remove wrong python and pip aliases

--- src/components/scripts/python/pyenv_init.sh
+++ src/components/scripts/python/pyenv_init.sh
@@ -4,6 +4,3 @@ export PYENV_ROOT="$HOME/.pyenv"
 export PATH="$PYENV_ROOT/shims:$PATH"
 
 if which pyenv > /dev/null; then eval "$(pyenv init -)"; fi
-
-alias python="$(pyenv which python)"
-alias pip="$(pyenv which pip)"
