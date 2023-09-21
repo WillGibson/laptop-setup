8a158f221f6286657cc40b60c23ffa9a388d3ee4
Will Gibson <8738245+WillGibson@users.noreply.github.com>
2023-09-21 14:36:13 +0100

    Stop git-delete-all-local-branches-except-main-develop-and-current trying to delete current branch

--- src/components/zshrc/aliases/git.sh
+++ src/components/zshrc/aliases/git.sh
@@ -9,7 +9,7 @@ alias gcd="gco development"
 alias gcwhatever='gaa && gc -m "Whatever $(date)" && ggpush'
 alias glog="glola"
 alias ghist="git log --graph --pretty='%Cred%h%Creset - %Cgreen%cI%Creset -%C(auto)%d%Creset %s %C(bold blue)<%an>%Creset'"
-alias git-delete-all-local-branches-except-main-develop-and-current='git branch | grep -v "master" | grep -v "main" | grep -v "develop" | xargs git branch -D'
+alias git-delete-all-local-branches-except-main-develop-and-current='git branch | grep -v "master" | grep -v "main" | grep -v "develop" | grep -v "$(git rev-parse --abbrev-ref HEAD)" | xargs git branch -D'
 alias gmm="${basePath}scripts/git/gmm.sh"
 alias gtc="${basePath}scripts/git/gtc.sh"
 alias gtp="git commit --allow-empty -m 'Trigger pipeline' && ggpush"
