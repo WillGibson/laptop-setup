eb30d2895f8a3b413ab77bb6741bb8ce6e4192f4
Will Gibson (Made Tech) <261894875+willgibson-madetech@users.noreply.github.com>
2026-03-11 17:16:02 +0000

    Drop alias docker-pull-all

--- src/components/zshrc/aliases/docker.sh
+++ src/components/zshrc/aliases/docker.sh
@@ -7,7 +7,6 @@ basePath="$( cd "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )/../../"
 alias docker-cleanup="${basePath}scripts/docker/docker-cleanup"
 # shellcheck disable=SC2139
 alias docker-brutal-cleanup="${basePath}scripts/docker/docker-brutal-cleanup"
-alias docker-pull-all='docker images |grep -v REPOSITORY|awk '"'"'{print $1}'"'"'|xargs -L1 docker pull'
 # shellcheck disable=SC2139
 alias docker-really-brutal-cleanup="${basePath}scripts/docker/docker-really-brutal-cleanup"
 
