d152580464984deb702126a1b0723e3238f05533
Will Gibson (Made Tech) <261894875+willgibson-madetech@users.noreply.github.com>
2026-03-11 17:25:08 +0000

    Small beer

--- src/components/commands/asdf.sh
+++ src/components/commands/asdf.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 ensure_asdf_is_installed() {
-    rm -f $HOME/.tool-versions
-    installApplicationHomebrewStyle "asdf"
+    if [ "${configOnly}" != "true" ]; then
+        rm -f "$HOME/.tool-versions"
+        installApplicationHomebrewStyle "asdf"
+    fi
     source "$(brew --prefix asdf)/libexec/asdf.sh"
     append_to_zshrc_parts 'source $(brew --prefix asdf)/libexec/asdf.sh'
     chmod +x "$(brew --prefix asdf)/libexec/asdf.sh"
@@ -12,9 +14,9 @@ installApplicationWithAsdf() {
     local applicationName="$1"
 
     if [[ "$2" != "" ]];then
-      local versionPrefix=":$2"
-      echo "$2"
-      echo "$versionPrefix"
+        local versionPrefix=":$2"
+        echo "$2"
+        echo "$versionPrefix"
     fi
 
     echo_heading "Installing ${applicationName} with asdf"
--- src/components/commands/ssh.sh
+++ src/components/commands/ssh.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 ensure_ssh_rsa_works() {
+    mkdir -p "$HOME/.ssh"
+    touch "$HOME/.ssh/ssh_config"
     append_text_to_file ~/.ssh/ssh_config "HostkeyAlgorithms +ssh-rsa" 1
     append_text_to_file ~/.ssh/ssh_config "PubkeyAcceptedAlgorithms +ssh-rsa" 1
 }
