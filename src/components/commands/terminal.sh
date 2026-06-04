75f51d0ea0b73503b512cc7d7807d6213100fc64
Will Gibson (Made Tech) <261894875+willgibson-madetech@users.noreply.github.com>
2026-06-04 07:35:48 +0100

    Add todo

--- src/components/commands/terminal.sh
+++ src/components/commands/terminal.sh
@@ -11,6 +11,7 @@ ensure_terminal_stuff_is_installed() {
     ensure_zsh_autosuggestions_are_installed
     ensure_zsh_completion_waiting_dots_are_used
     ensure_correct_ohmyzsh_theme_is_used "${basePath}/components/ohmyzsh/willgibson.zsh-theme" "willgibson"
+    # Todo: Confirm this command works
     update_file_line_in_situ ~/.zshrc 'plugins=(git)' 'plugins=(docker git kubectl zsh-autosuggestions)'
     append_to_zshrc_parts "ZSH_AUTOSUGGEST_MANUAL_REBIND=false" # This might need to go before it loads ohmyzsh
 }
